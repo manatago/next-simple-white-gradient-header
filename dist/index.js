a7533237f8d254e4433aec0da9e295770429ff15
manatago <satoshi@tagomori.biz>
2022-10-22 14:51:56 +0900

    third

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,22 @@ exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./styles.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var NextSimpleWhiteGradientHeader = function NextSimpleWhiteGradientHeader() {
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello from My Component");
+var NextSimpleWhiteGradientHeader = function NextSimpleWhiteGradientHeader(props) {
+  var defaultRotate = '90deg';
+  var defaultGradientColor = '#FFFFFF';
+  var headerStyle = {
+    backgroundImage: 'url(' + props.backgroundImagePath + ')'
+  };
+  var wrapperStyle = {
+    background: 'linear-gradient(' + (props.gradientRotate ? props.gradientRotate : defaultRotate) + ',' + (props.gradientColor ? props.gradientColor : defaultGradientColor) + (props.gradientStartOpacity ? props.gradientStartOpacity : 'FF') + ' 0%,' + (props.gradientColor ? props.gradientColor : defaultGradientColor) + (props.gradientEndOpacity ? props.gradientEndOpacity : '00') + ' 100%)'
+  };
+  return /*#__PURE__*/_react["default"].createElement("header", {
+    style: headerStyle,
+    className: "nextSimpleWhiteGradientHeader"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    style: wrapperStyle,
+    className: "nextSimpleWhiteGradientHeaderWraper"
+  }, props.children));
 };
 var _default = NextSimpleWhiteGradientHeader;
 exports["default"] = _default;
