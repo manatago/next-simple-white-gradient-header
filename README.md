4f5014d4a1fd0fa4cae6ffde41dd6890039d1c61
manatago <satoshi@tagomori.biz>
2022-10-22 15:19:44 +0900

    fifth

---
+++ README.md
@@ -0,0 +1,45 @@
+# このコンポーネントの使い方
+
+## インストール方法
+```
+npm install @tagosan/next-simple-white-gradient-header
+```
+で読み込みます。
+
+## 使い方
+```
+import NextSimpleWhiteGradientHeader from '@tagosan/next-simple-white-gradient-header'
+```
+こんな感じでコンポーネントを読み込みます。
+
+##　パラメーター
+### backgroundImagePath
+背景画像のパスです。これだけは必須です。
+
+### gradientRotate
+回転角度をdegで指定します。
+gradientRotate='90deg'のように指定します。
+
+### gradientColor
+背景の色を指定します。
+gradientColor='#FFFFFF'のような感じです。
+rgb(255,255,255)
+white
+#FFF
+のようなスタイルではダメです。
+
+### gradientStartOpacity
+開始地点の背景の透明度を２桁の16進数で指定します。
+gradientStartOpacity='FF'
+のような感じです。
+
+### gradientEndOpacity
+終了地点の背景の透明度を２桁の16進数で指定します。
+gradientEndOpacity='00'
+のような感じです。
+
+### height
+高さを指定します。
+初期値は400pxが入ります。
+height='300px'
+のように指定してください。
--- dist/index.js
+++ dist/index.js
@@ -5,15 +5,20 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
-require("./styles.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var NextSimpleWhiteGradientHeader = function NextSimpleWhiteGradientHeader(props) {
   var defaultRotate = '90deg';
   var defaultGradientColor = '#FFFFFF';
   var headerStyle = {
-    backgroundImage: 'url(' + props.backgroundImagePath + ')'
+    backgroundImage: 'url(' + props.backgroundImagePath + ')',
+    height: props.height ? props.height : '400px',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
   };
   var wrapperStyle = {
+    width: '100%',
+    height: '100%',
     background: 'linear-gradient(' + (props.gradientRotate ? props.gradientRotate : defaultRotate) + ',' + (props.gradientColor ? props.gradientColor : defaultGradientColor) + (props.gradientStartOpacity ? props.gradientStartOpacity : 'FF') + ' 0%,' + (props.gradientColor ? props.gradientColor : defaultGradientColor) + (props.gradientEndOpacity ? props.gradientEndOpacity : '00') + ' 100%)'
   };
   return /*#__PURE__*/_react["default"].createElement("header", {
